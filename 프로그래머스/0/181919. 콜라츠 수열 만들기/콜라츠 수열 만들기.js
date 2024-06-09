f0d82a2f03ec2a2d89b677803828a9702fe23b03
yammyam <96424434+yammyam@users.noreply.github.com>
2024-06-09 21:26:25 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -11,3 +11,10 @@ function solution(n) {
     }
     return res;
 }
+//재귀함수
+// function solution(n, arr = []) {
+//     arr.push(n)
+//     if (n === 1) return arr
+//     if (n % 2 === 0) return solution(n / 2, arr)
+//     return solution(3 * n + 1, arr)
+// }
