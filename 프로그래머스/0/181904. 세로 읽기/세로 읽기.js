5e23491b34f1190248ba0b8fdf63323bd413fc15
yammyam <96424434+yammyam@users.noreply.github.com>
2024-06-11 05:08:19 +0900

    [level 0] Title: 세로 읽기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,12 +1,3 @@
 function solution(my_string, m, c) {
-    let answer = [];
-    let str = ""
-    for(let i = 0 ; i< my_string.length ; i++){
-        if(i%m !== 0 || i === 0)
-            str += my_string[i];
-        else{
-            str += " "+my_string[i];
-        }
-    }
-    return str.split(" ").map((item)=>item[c-1]).join("")
+    return [...my_string].filter((item,i)=>i%m === c-1).join("")
 }
