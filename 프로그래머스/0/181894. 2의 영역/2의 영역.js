a9912d6f4ac76739263544d3fc3fc059995d52a5
yammyam <96424434+yammyam@users.noreply.github.com>
2024-06-14 21:44:53 +0900

    [level 0] Title: 2의 영역, Time: 3.51 ms, Memory: 41.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    let res = [];
+    let start = arr.indexOf(2);
+    let end = arr.lastIndexOf(2);
+    if(start === -1)
+        res.push(-1);
+    else if(start !== -1 && end !== -1){
+        for(let i = start; i<=end; i++){
+            res.push(arr[i]);
+        }
+    }else{
+        res.push(2);
+    }
+        
+    return res;
+}
