6ac4be56231f1ff4a11a6bbaef4386e470a3b630
yammyam <96424434+yammyam@users.noreply.github.com>
2024-06-15 23:37:47 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -0,0 +1,17 @@
+function solution(rank, attendance) {
+    let obj = {};
+    let arr = rank.map((i,index)=>{
+        if(attendance[index]){
+          return i;
+        }else{
+        return 101;
+        }
+    })
+    for(let i = 0 ; i < arr.length; i++){
+        obj[i] = arr[i];
+    }
+    const entries = Object.entries(obj);
+    entries.sort((a, b) => a[1] - b[1]);
+    let res = entries.slice(0,3)
+    return 10000*Number(res[0][0]) + 100*Number(res[1][0]) + Number(res[2][0]);
+}
