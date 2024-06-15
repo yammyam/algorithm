d6c1666d92b0c34cbcd1b50b5504fcec86d911a8
yammyam <96424434+yammyam@users.noreply.github.com>
2024-06-15 20:10:03 +0900

    [level 0] Title: 배열 만들기 6, Time: 3.43 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+    let stk = [];
+    for(let i = 0 ; i < arr.length ; i++){
+        if(stk.length === 0){
+             stk.push(arr[i])
+       }
+        else if(stk.length !== 0 && stk[stk.length-1] === arr[i]){
+            stk.pop();
+        }
+        else{
+            stk.push(arr[i])
+        }    
+    }
+    
+    if(stk.length === 0)
+        return [-1];
+    else
+        return stk;
+}
