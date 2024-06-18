a9454ded7dca7fbe9ffc35043f43f3c950c15cfa
yammyam <96424434+yammyam@users.noreply.github.com>
2024-06-18 21:36:49 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,24 +1,22 @@
 function solution(progresses, speeds) {
-    // [7,3,9]
     let arr = progresses.map((i,index)=>{
         if((100-i)/speeds[index] === Math.floor((100-i)/speeds[index]))
             return (100-i)/speeds[index]
         else
             return Math.ceil((100-i)/speeds[index]);
     })
-    // 객체로만들고 오름차순 밸류반납
     let answer = [arr[0]];
     for(let i = 1 ; i<arr.length ; i++){
-        if(arr[i-1]>arr[i]){ //arr이 779
+        if(arr[i-1]>arr[i]){
             arr[i] = arr[i-1];
         }
         answer.push(arr[i]);
     }
     let counts = answer.reduce((acc, val) => {
-    acc[val] = (acc[val] || 0) + 1;
-    return acc;
-}, {});
+        acc[val] = (acc[val] || 0) + 1;
+        return acc;
+    }, {});
 
-let result = Object.keys(counts).sort((a, b) => a - b).map(key => counts[key]);
+    let result = Object.keys(counts).sort((a, b) => a - b).map(key => counts[key]);
     return result;
 }
