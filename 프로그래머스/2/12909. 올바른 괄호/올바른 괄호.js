5a51403c2c7f0ab6435370140a71cd140fb64c6f
yammyam <96424434+yammyam@users.noreply.github.com>
2024-06-22 00:30:42 +0900

    [level 2] Title: 올바른 괄호, Time: 26.89 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -29,16 +29,31 @@
 
 //     return answer;
 // }
-const solution = (s)=>{
-    let stack = [];
-    for(let i = 0 ; i < s.length ; i++){
-        if(s[i] === "("){
-            stack.push("(");
-        }else{
-            if(stack.length === 0)
-                return false;
-            stack.pop();
+// const solution = (s)=>{
+//     let stack = [];
+//     for(let i = 0 ; i < s.length ; i++){
+//         if(s[i] === "("){
+//             stack.push("(");
+//         }else{
+//             if(stack.length === 0)
+//                 return false;
+//             stack.pop();
+//         }
+//     }
+//     return stack.length === 0;
+// }
+function solution(s)
+{
+    let answer = true;
+    let count = 0;
+    for (let i of s)
+    {
+        i === '(' ? count++ : count--;
+        if (count < 0)
+        {
+            answer = false;
+            break;
         }
     }
-    return stack.length === 0;
+    return count !== 0 ? false : answer;
 }
