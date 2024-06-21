93a2e4a6a3b8bfb1b7ca423de5b927f1420c9098
yammyam <96424434+yammyam@users.noreply.github.com>
2024-06-22 00:26:39 +0900

    [level 2] Title: 올바른 괄호, Time: 24.06 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,51 +1,44 @@
-// function check(a,repeat, arr){
-//     for(let i = a ; i < repeat ;i++){//2~3
-//         if(arr[i] !== ")")
+// function check(a, repeat, arr) {
+//     for (let i = a; i < a + repeat; i++) {
+//         if (arr[i] !== ")")
 //             return false;
 //     }
 //     return true;
 // }
-// function solution(s){
+
+// function solution(s) {
 //     let arr = s.split("");
-//     let repeat = 1;
+//     let repeat = 0;
 //     let answer = true;
-//     for(let i = 1 ; i < s.length; i++){
-//         if(arr[i-1] === "(" && arr[i] === ")"){
-//             answer = check(i,i+repeat,arr)
-//         }else
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (arr[i] === "(") {
 //             repeat++;
-//         repeat = 1;
+//         } else if (arr[i] === ")") {
+//             if (repeat === 0) {
+//                 answer = false;
+//                 break;
+//             }
+//             repeat--;
+//         }
 //     }
+
+//     if (repeat !== 0) {
+//         answer = false;
+//     }
+
 //     return answer;
 // }
-function check(a, repeat, arr) {
-    for (let i = a; i < a + repeat; i++) {
-        if (arr[i] !== ")")
-            return false;
-    }
-    return true;
-}
-
-function solution(s) {
-    let arr = s.split("");
-    let repeat = 0; // 반복 횟수 초기화
-    let answer = true;
-
-    for (let i = 0; i < s.length; i++) {
-        if (arr[i] === "(") {
-            repeat++; // 여는 괄호의 개수를 증가
-        } else if (arr[i] === ")") {
-            if (repeat === 0) { // 여는 괄호가 없는 상태에서 닫는 괄호가 나올 경우
-                answer = false;
-                break;
-            }
-            repeat--; // 여는 괄호의 개수를 감소
+const solution = (s)=>{
+    let stack = [];
+    for(let i = 0 ; i < s.length ; i++){
+        if(s[i] === "("){
+            stack.push("(");
+        }else{
+            if(stack.length === 0)
+                return false;
+            stack.pop();
         }
     }
-
-    if (repeat !== 0) { // 모든 반복이 끝난 후 여는 괄호가 남아 있는 경우
-        answer = false;
-    }
-
-    return answer;
+    return stack.length === 0;
 }
