1cc3851e1ba18b85cfe33782dd017c6935f12645
yammyam <96424434+yammyam@users.noreply.github.com>
2026-01-27 21:45:20 +0900

    [level 1] Title: 폰켓몬, Time: 0.73 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,3 @@
-// function solution(nums) {
-//     let leng = nums.length/2;
-//     let arr = Array.from(new Set(nums));
-//     let count = 0;
-//     if(arr.length > leng){
-//         return leng;
-//     }else{
-//         return arr.length;
-//     }
-// }
 const solution = (nums) => {
     let l = nums.length/2;
     let unique = [...new Set(nums)];
