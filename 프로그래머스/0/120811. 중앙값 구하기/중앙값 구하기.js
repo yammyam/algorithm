264aca7fae22a3b0120feee9e61a54be8df5ba43
yammyam <96424434+yammyam@users.noreply.github.com>
2026-01-29 03:08:52 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,4 +1,4 @@
 const solution = (array) => {
-  array.sort((a,b)=> a-b);
-  return array[(array.length-1)/2]
+    return array.sort((a,b)=> a-b)[Math.floor(array.length / 2)]
+    //(array.length - 1) / 2
 }
