8fcaff1e599b2c7b75e6151800ba4a0bf900e9b0
yammyam <96424434+yammyam@users.noreply.github.com>
2026-01-28 23:24:58 +0900

    [level 0] Title: 배열의 유사도, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/README.md
+++ 프로그래머스/0/120903. 배열의 유사도/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열의 유사도 - 120903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120903?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120903) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 28일 23:19:54
+2026년 01월 28일 23:24:56
 
 ### 문제 설명
 
--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,8 +1,16 @@
 const solution = (s1, s2) => {
-    let obj = {};
-    for(let i of s1){
-        if(s2.includes(i))
-           obj[i] = (obj[i]|| 1)
-    }
-    return Object.keys(obj).length
+    // let obj = {};
+    // for(let i of s1){
+    //     if(s2.includes(i))
+    //        obj[i] = (obj[i]|| 1)
+    // }
+    // return Object.keys(obj).length
+    // 위는 해시처럼 보이나 시간복잡도가 n*n
+    let set  = new Set(s1);
+    let answer = 0;
+    for(let i of s2)
+        if(set.has(i))
+            answer++;
+    return answer;
+    // set은 내부적으로 해시 테이블 구조이기때문에 위치바로찾기가 가능 => 해시알고리즘
 }
