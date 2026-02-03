778596ed425ea48557b2f07ca3fc173c78899858
yammyam <96424434+yammyam@users.noreply.github.com>
2026-02-04 00:51:30 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,13 +1,16 @@
-const solution = (my_string) => {
-    let code = my_string.split(" ")
-    let answer = Number(code[0])
-    for(let i = 1; i<code.length;i++){
-        if(code[i]==="+"){
-            answer += Number(code[i+1])
-        }
-        else if(code[i]==="-"){
-            answer -= Number(code[i+1])
-        }
-    }
-    return answer;
+const solution = (my_string) =>{
+    return my_string.replace(/ - /g," + -").split(" + ").reduce((a,c)=>a+Number(c),0)
 }
+// const solution = (my_string) => {
+//     let code = my_string.split(" ")
+//     let answer = Number(code[0])
+//     for(let i = 1; i<code.length;i++){
+//         if(code[i]==="+"){
+//             answer += Number(code[i+1])
+//         }
+//         else if(code[i]==="-"){
+//             answer -= Number(code[i+1])
+//         }
+//     }
+//     return answer;
+// }
