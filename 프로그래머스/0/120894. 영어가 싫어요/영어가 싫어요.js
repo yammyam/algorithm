303f10f654d23802e967a0902fb9db3f5dab5318
yammyam <96424434+yammyam@users.noreply.github.com>
2026-02-04 02:03:23 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,6 +1,5 @@
-function solution(numbers) {
+const solution = (numbers) => {
     const arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    let answer = "";
     arr.forEach((item, i)=>{
           numbers = numbers.split(item).join(i);
     })
