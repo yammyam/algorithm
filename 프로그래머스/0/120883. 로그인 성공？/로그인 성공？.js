d4968056dcddccb61640b1d3d28890a3b98d0857
yammyam <96424434+yammyam@users.noreply.github.com>
2026-02-09 01:26:52 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,11 +1,17 @@
-const solution = (id_pw, db) => {
-    let [id,pw] = id_pw;
-    let check = false;
-    for(let item of db){
-        if(id===item[0])
-            check = !check
-        if(id===item[0]&&pw===item[1])
-            return "login"
-    }
-    return check ? "wrong pw" : "fail"
+const solution = ([id,pw], db) => {
+    const findId = db.find(([dbid]) => dbid === id)
+    //find는 일치하는것의 요소를 직접반환, 구조분해할당 두개받아야되지만 id만받아서 쓸수있음.여긴 2차원배열이니 요소를 id만배열하는게아니라 2차원배열이니까 안쪽 배열 하나를 반환해줌 ex)["meosseugi", "1234"]
+    if(!findId)return "fail";
+    return findId[1] === pw ? "login" : "wrong pw";
 } 
+// const solution = (id_pw, db) => {
+//     let [id,pw] = id_pw;
+//     let check = false;
+//     for(let item of db){
+//         if(id===item[0])
+//             check = true
+//         if(id===item[0]&&pw===item[1])
+//             return "login"
+//     }
+//     return check ? "wrong pw" : "fail"
+// } 
