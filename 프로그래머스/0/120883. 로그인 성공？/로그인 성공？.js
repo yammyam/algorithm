7bd6b3181be2989b1f9a056813794611a412c1b6
yammyam <96424434+yammyam@users.noreply.github.com>
2026-02-09 01:14:18 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,11 @@
+function solution(id_pw, db) {
+    let [id,pw] = id_pw;
+    let check = false;
+    for(let item of db){
+        if(id===item[0])
+            check = !check
+        if(id===item[0]&&pw===item[1])
+            return "login"
+    }
+    return check ? "wrong pw" : "fail"
+} 
