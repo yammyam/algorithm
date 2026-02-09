198cb745a85c53ebcf64dd8a4af3c4db32a8a12b
yammyam <96424434+yammyam@users.noreply.github.com>
2026-02-10 00:50:22 +0900

    [level 0] Title: 다항식 더하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -3,15 +3,13 @@ const solution = (polynomial) => {
     let c = 0;
     let arr = polynomial.split(" + ")
     for(let i = 0 ; i <arr.length; i++){
-        if(arr[i] === "x"){
-            x+=1;
-        }
-        if(arr[i].includes("x") && arr[i].length!==1){ //계수붙은x일때
-            x += Number(arr[i].split("x")[0])
-        }
-        if(!Number.isNaN(Number(arr[i]))){//숫자일때
+        if(arr[i].includes("x")){ //계수붙은x일때
+            x += arr[i] === "x" ? 1: Number(arr[i].split("x")[0])
+        }else
             c+=Number(arr[i])
-        }
+        // if(!Number.isNaN(Number(arr[i]))){//숫자일때
+        //     c+=Number(arr[i])
+        // }
     }
     if(x===0 && c===0)
         return "";
@@ -22,3 +20,27 @@ const solution = (polynomial) => {
     else if(x!==0 && c!==0)
         return x===1 ? `x + ${c}`: `${x}x + ${c}`
 }
+// const solution = (polynomial) => {
+//     let x = 0;
+//     let c = 0;
+//     let arr = polynomial.split(" + ")
+//     for(let i = 0 ; i <arr.length; i++){
+//         if(arr[i] === "x"){
+//             x+=1;
+//         }
+//         if(arr[i].includes("x") && arr[i].length!==1){ //계수붙은x일때
+//             x += Number(arr[i].split("x")[0])
+//         }
+//         if(!Number.isNaN(Number(arr[i]))){//숫자일때
+//             c+=Number(arr[i])
+//         }
+//     }
+//     if(x===0 && c===0)
+//         return "";
+//     else if(x===0 && c!==0)
+//         return `${c}`;
+//     else if(x!==0 && c===0)
+//         return x ===1 ? "x" : `${x}x`;
+//     else if(x!==0 && c!==0)
+//         return x===1 ? `x + ${c}`: `${x}x + ${c}`
+// }
