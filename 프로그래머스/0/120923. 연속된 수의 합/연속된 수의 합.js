11c92cf187040318efc127f5ceb9c0b8d05689cb
yammyam <96424434+yammyam@users.noreply.github.com>
2026-02-10 02:21:12 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,4 +1,5 @@
-function solution(num, total) {
+const solution = (num, total) => {
+    // 시작값의 공식을 세우기 1~n까지의 합 등차수열 합공식과 수를 쭉써서 덧셈이 어떻게표현되는지 통해 구함
     let start = (total - ((num*(num-1))/2))/num;
     return Array.from({length: num},(_,i)=>start+i)
 }
