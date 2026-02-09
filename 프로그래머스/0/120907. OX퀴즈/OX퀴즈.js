edb674a834dd62096deee5cb5e92b08059823d1c
yammyam <96424434+yammyam@users.noreply.github.com>
2026-02-10 01:54:21 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,11 +1,24 @@
 const solution = (quiz) =>{
     let arr = quiz.map((item)=> item.split(" "));
     return arr.map((item)=>{
+        //const [x,op,y,,z]  = item이렇게 구조분해할당으로도 받아오기가능
         return item[1] === "+" ? 
         Number(item[0])+Number(item[2]) === Number(item[4])? "O":"X" :
         Number(item[0])-Number(item[2]) === Number(item[4])? "O":"X"
     })
 }
+// *가독성 좋은 풀이
+// const solution = (quiz) => {
+//   return quiz.map(expr => {
+//     const [x, op, y, , z] = expr.split(" ");
+//     const result = op === "+" ? Number(x) + Number(y)
+//                               : Number(x) - Number(y);
+//     return result === Number(z) ? "O" : "X";
+//   });
+// };
+
+
+// *아래는 처음내가 푼것
 // const solution = (quiz) => {
 //     let arr = quiz.map((item)=> item.replace(/ - /g," + -").split(" "));
 //     //5 - -15일때 5 - --15로 버그발생!!!!!
