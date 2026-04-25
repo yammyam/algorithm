e607c6f30a39e73503c526caaafcb007c2f6089b
yammyam <96424434+yammyam@users.noreply.github.com>
2026-04-25 23:52:36 +0900

    [level 2] Title: 가장 큰 수, Time: 193.41 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -3,11 +3,11 @@ const  solution = (numbers) =>{
         const ap = `${a}`+`${b}`;
         const bp = `${b}`+`${a}`;
         if(Number(ap)>Number(bp))
-            return -1;
+            return -1;//a가 b보다 앞에와라
         if (Number(ap) < Number(bp)) 
             return 1;
         return 1;
     })
-    return answer[0]!==0?answer.join(""): "0";
+    return answer[0]!==0 ? answer.join("") : "0";
 }
-//예외도 잊지말것.
+//0이라는 예외도 잊지말것.
